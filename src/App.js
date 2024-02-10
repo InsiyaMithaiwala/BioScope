4368568258af7992327c808c5cac044611af5be1
Raaghav Mehta <75270837+RaaghavMehta17@users.noreply.github.com>
2024-02-10 12:34:35 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Home from './components/Home';
+import FeatureCard from './components/feature-card';
+import Question1 from './components/question1';
+import './App.css';
+import NotFound from './components/not-found';
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <FeatureCard />
+     
+     
+    </div>
+  );
+}
+
+export default App;
